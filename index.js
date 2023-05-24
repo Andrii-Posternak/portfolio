70092ff21a8ba46bfa7ef98820df7f4b2167e6d0
Andrii-Posternak <panposternak@gmail.com>
2023-05-24 11:31:21 +0300

    added ataptive layout

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
+const windowWidth = document.documentElement.clientWidth;
+console.log(windowWidth);
+
 // if (ScrollTrigger.isTouch !== 1) {
 ScrollSmoother.create({
   wrapper: ".wrapper",
@@ -24,46 +27,89 @@ gsap.fromTo(
 
 let itemsL = gsap.utils.toArray(".gallery__left .gallery__item");
 
-itemsL.forEach((item) => {
-  gsap.fromTo(
-    item,
-    {
-      opacity: 0,
-      x: -70,
-    },
-    {
-      opacity: 1,
-      x: 0,
-      scrollTrigger: {
-        trigger: item,
-        start: "-1150",
-        end: "-100",
-        scrub: true,
+if (windowWidth < 480) {
+  itemsL.forEach((item) => {
+    gsap.fromTo(
+      item,
+      {
+        opacity: 0,
+        x: -100,
       },
-    }
-  );
-});
+      {
+        opacity: 1,
+        x: 0,
+        scrollTrigger: {
+          trigger: item,
+          start: "-1300",
+          end: "-900",
+          scrub: true,
+        },
+      }
+    );
+  });
+} else {
+  itemsL.forEach((item) => {
+    gsap.fromTo(
+      item,
+      {
+        opacity: 0,
+        x: -100,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        scrollTrigger: {
+          trigger: item,
+          start: "-1150",
+          end: "-100",
+          scrub: true,
+        },
+      }
+    );
+  });
+}
 
 let itemsR = gsap.utils.toArray(".gallery__right .gallery__item");
 
-itemsR.forEach((item) => {
-  gsap.fromTo(
-    item,
-    {
-      opacity: 0,
-      x: 50,
-    },
-    {
-      opacity: 1,
-      x: 0,
-      scrollTrigger: {
-        trigger: item,
-        start: "-850",
-        end: "-100",
-        scrub: true,
+if (windowWidth < 480) {
+  itemsR.forEach((item) => {
+    gsap.fromTo(
+      item,
+      {
+        opacity: 0,
+        x: 100,
       },
-    }
-  );
-});
-
+      {
+        opacity: 1,
+        x: 0,
+        scrollTrigger: {
+          trigger: item,
+          start: "-1300",
+          end: "-900",
+          scrub: true,
+        },
+      }
+    );
+  });
+} else {
+  itemsR.forEach((item) => {
+    gsap.fromTo(
+      item,
+      {
+        opacity: 0,
+        x: 100,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        scrollTrigger: {
+          trigger: item,
+          start: "-850",
+          end: "-100",
+          scrub: true,
+        },
+      }
+    );
+  });
+}
 // }
